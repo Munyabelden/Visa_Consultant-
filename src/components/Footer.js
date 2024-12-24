2a09a64003b16362557f8039462956bcea150bac
unknown <munyaradzi045@gmail.com>
2024-12-24 12:35:38 +0200

    Change emails

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,13 +16,13 @@ const Footer = () => {
           <div>
             <div>
               <h4>Tatenda</h4>
-              <a href="mailto:tatenda862@gmail.com?subject=Inquiry&body=Visa Consultation Enquiry">tatenda862@gmail.com</a><br />
+              <a href="mailto:tatenda@visionaryvisaconsultation.co.za?subject=Inquiry&body=Visa Consultation Enquiry">tatenda@visionaryvisaconsultation.co.za</a><br />
               <a href="https://wa.me/+27720663578" target="_blank" rel="noreferrer"><i className="fa-brands fa-whatsapp"></i></a>
               <span>or call +27 72 066 3578</span>
             </div>
             <div>
               <h4>Henry</h4>
-              <a href="mailto:your-email@example.com?subject=Inquiry&body=Visa Consultation Enquiry">tatenda862@gmail.com</a><br />
+              <a href="mailto:henry@visionaryvisaconsultation.co.za?subject=Inquiry&body=Visa Consultation Enquiry">henry@visionaryvisaconsultation.co.za</a><br />
               <a href="https://wa.me/+27 64 225 6536" target="_blank" rel="noreferrer"><i className="fa-brands fa-whatsapp"></i></a>
               <span>or call +27 64 225 6536</span>
             </div>
