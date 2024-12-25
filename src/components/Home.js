b03c9fe4ca9b1e2ef6a80c934efcec6a1f0c2950
unknown <munyaradzi045@gmail.com>
2024-12-25 10:44:17 +0200

    Change emails

--- src/components/Home.js
+++ src/components/Home.js
@@ -47,37 +47,31 @@ const Home = () => {
           <div className="country uk">
             <div>
               <span>United Kingdom</span>
-              <button className="country-btn">more info</button>
             </div>
           </div>
           <div className="country usa">
             <div>
               <span>USA</span>
-              <button className="country-btn">more info</button>
             </div>
           </div>
           <div className="country can">
             <div>
               <span>Canada</span>
-              <button className="country-btn">more info</button>
             </div>
           </div>
           <div className="country aust">
             <div>
               <span>Australia</span>
-              <button className="country-btn">more info</button>
             </div>
           </div>
           <div className="country nz">
             <div>
               <span>New Zealand</span>
-              <button className="country-btn">more info</button>
             </div>
           </div>
           <div className="country fran">
             <div>
               <span>France</span>
-              <button className="country-btn">more info</button>
             </div>
           </div>
         </div>
